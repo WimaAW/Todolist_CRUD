7cc750575f8a8b577e19b40c60247fd560a4ab2c
WimaAW <57026027+WimaAW@users.noreply.github.com>
2024-10-23 19:11:04 +0700

    Import dynamo db

--- lambda_function.py
+++ lambda_function.py
@@ -1,1 +1,10 @@
+import json
+import boto3
+from decimal import Decimal
+import uuid  # Untuk menghasilkan ID unik
+
+# Inisialisasi client dan resource DynamoDB
+client = boto3.client('dynamodb')
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table('todo-list-items')  # Nama tabel DynamoDB untuk to-do list
 
